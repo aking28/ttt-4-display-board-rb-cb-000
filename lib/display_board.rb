a3a7589d3eeb44cd2c2910ad4e904e1a6460fdd6
Alexandra King <aking28@me.com>
2017-11-06 16:59:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board(board = ["O", "X", " ", " ", "X", " ", "X", "O", " "])
-  puts "#{board[0]}|#{board[1]}|   "
+  puts "   |   |   "
   puts "-----------"
-  puts "  |#{board[4]}"|   "
+  puts "   |   |   "
   puts "-----------"
-  puts "#{board[6]},#{board[7]}""   |   |   "
+  puts "   |   |   "
 end
   display_board
