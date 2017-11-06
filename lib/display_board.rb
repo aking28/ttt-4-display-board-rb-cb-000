cd0e35e307782ceb9af94c189dc2e92001988fd2
Alexandra King <aking28@me.com>
2017-11-06 16:44:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,5 +4,6 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  capture_puts{ display_board(board) }
 end
   display_board
