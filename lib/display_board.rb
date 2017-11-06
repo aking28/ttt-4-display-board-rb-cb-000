e01b3817bec441104c14f1526f041845bb3dba6f
Alexandra King <aking28@me.com>
2017-11-06 16:49:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board(board = [" "," "," "," "," "," "," "," "," "])
+def display_board(board = ["O", "X", " ", " ", "X", " ", "X", "O", " "])
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
